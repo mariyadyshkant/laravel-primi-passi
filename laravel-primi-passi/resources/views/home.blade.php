8f56657e1c7278e2f0313f8439aaad82f69523b1
mariyadyshkant <mariya.dyshkant@gmail.com>
2026-03-20 11:57:56 +0100

    Add home view template with Bootstrap navigation

---
+++ laravel-primi-passi/resources/views/home.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primi passi con Laravel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class= "bg-light">
+    <nav class="navbar bg-light">
+        <div class="container border-bottom border-1 border-primary ">
+            <a class="navbar-brand text-primary fs-1" href="#">Laravel</a>
+        </div>
+        <ul class="nav flex-column nav-pills ms-4">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Scopri</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contatti</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Assistenza</a>
+            </li>
+        </ul>
+    </nav>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
